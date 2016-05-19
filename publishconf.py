f3bc142b846b4b7e4f6e411c2883d644b6f4c5eb
Victor Gau <victorgau@gmail.com>
2016-05-20 06:46:58 +0800

    Update several files

--- publishconf.py
+++ publishconf.py
@@ -33,18 +33,28 @@ PATH = 'content'
 TIMEZONE = 'Asia/Taipei'
 DEFAULT_LANG = u'en'
 
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('iPython','http://ipython.org/'),
-         ('Flask','http://flask.pocoo.org/'),
+         ('IPython','http://ipython.org/'),
+         ('Pandas','http://pandas.pydata.org/'),
+         ('matplotlib','http://matplotlib.org/'),
+         ('Scikit-Learn','http://scikit-learn.org/stable/'),
+         ('Statsmodel','http://statsmodels.sourceforge.net/'),
          ('Django','https://www.djangoproject.com/'),
+         ('Flask','http://flask.pocoo.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('nltk','http://www.nltk.org/'),
+         ('jieba','https://github.com/fxsjy/jieba'),
+         ('gensim','https://radimrehurek.com/gensim/'),
          ('PyCharm Guide','https://www.jetbrains.com/pycharm/quickstart/'),
          ('RStudio','https://www.rstudio.com/'),)
 
 # Social widget
-SOCIAL = (('Github', 'http://github.com/victorgau'),)
+SOCIAL = (('Github', 'http://github.com/victorgau'),
+          ('BitBucket', 'https://bitbucket.org/victorgau'),)
 
 DISPLAY_TAGS_INLINE = True
 
@@ -57,7 +67,13 @@ THEME = "pelican-themes/pelican-bootstrap3"
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['render_math', 'liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.youtube', 'liquid_tags.vimeo',
-           'liquid_tags.include_code', 'liquid_tags.notebook', 'tag_cloud']
+           'liquid_tags.include_code', 'liquid_tags.notebook', 'tag_cloud', 'tipue_search']
 
 YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+
+BANNER = 'images/free_banner_red.jpg'
+BANNER_SUBTITLE = 'Just Do It!'
+
+ABOUT_ME = u"""突然發現自己對於股票投機的興趣，到了不做不可的程度，所以弄一個網站來記錄一些心得。"""
+AVATAR = 'images/victor.jpg'
